82aaaf09c4f66da96085f90c93dbad9a2b1e5455
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 00:26:08 +0800

    sort function

--- slection.py
+++ slection.py
@@ -1,1 +1,8 @@
 array ={17,51,52,31,28,47,50,61,43,65}
+
+def selection_sort(array):
+    for i in range(0, len(array)-1):
+        min=i
+        for j in range(i+1, len(array)):
+            if array[j] < array [min]:
+                min=j
