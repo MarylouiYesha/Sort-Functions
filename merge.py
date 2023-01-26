394cf91c7cf9e3b674772c74ee51aef53b033c14
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 05:51:04 +0800

    marged array

--- merge.py
+++ merge.py
@@ -3,4 +3,30 @@ def merge_sort(arrage):
         left_array = array[:len(array)//2]
         right_array = array[len(array)//2:]
 
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        i = 0 
+        j = 0 
+        k = 0 
+
+        while i < len(left_array) and j <len(right_array):
+            if left_array[i] < right_array[j]:
+                arrage[k] = left_array [i]
+                i += 1
+            else:
+                arrage[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i <len(left_array):
+            arrage[k] = left_array[i]
+            i += 1
+            k += 1
+        
+        while j < len(right_array):
+            array[k] = right_array[j]
+            j += 1
+            k += 1
+            
 array =[17,51,52,31,28,47,50,61,43,65]
