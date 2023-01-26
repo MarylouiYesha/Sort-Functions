840e0938fd5e879f166c23bf082d7b2ac930eba8
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 04:49:47 +0800

    final edit

--- selection.py
+++ selection.py
@@ -9,7 +9,7 @@ def selection_sort(array,size):
         
         (array[i], array[min]) = (array[min], array[i])
 
-print("Array in Ascending Order")
+print("Array in Ascending Order using Selection sort")
 array =[17,51,52,31,28,47,50,61,43,65]
 size = len(array)
 selection_sort(array,size)
