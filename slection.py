3b57510ec422fdc2243936f08934b682ac0265cc
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 01:09:14 +0800

    final

--- slection.py
+++ slection.py
@@ -1,13 +1,17 @@
-array ={17,51,52,31,28,47,50,61,43,65}
+
 
 def selection_sort(array):
-    for i in range(0, len(array)-1):
+
+    for i in range(5):
         min=i
-        for j in range(i+1, len(array)):
+        for j in range(i, 10):
             if array[j] < array [min]:
                 min=j
         
-        array[i], array[min]=array[min], array[i]
+        temp = array[i]
+        array[i] = array[min]
+        array[min] = temp
 
+array ={17,51,52,31,28,47,50,61,43,65}
 selection_sort(array)
 print(array)
