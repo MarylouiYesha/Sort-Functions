e6b5dd85782dc454403fac57723cb86a685d580c
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 03:43:15 +0800

    bubble function

--- bubble.py
+++ bubble.py
@@ -1,6 +1,11 @@
 def bubble_sort(array):
-    size = len (array)
+    num = len (array)
     swapped = False
+
+    for i in range(num-1):
+        for j in range (0, num-i-1):
+            if array[j] > array[j +1]:
+                swapped = True
 
 
 array =[17,51,52,31,28,47,50,61,43,65]
