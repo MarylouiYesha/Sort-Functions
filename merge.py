61010ec0b1fe71af92111bd2c89d856269e1a6fe
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 06:48:55 +0800

    divide array

--- merge.py
+++ merge.py
@@ -1,4 +1,11 @@
 def mergeSort(array):
     if len(array) > 1:
+        mid = len(array)//2
+        Left_array = array[:mid]
+        Right_array = array[mid:]
+        mergeSort(Left_array)
+        mergeSort(Right_array)
+
+        i = j = k = 0
 
 array = [17,51,52,31,28,47,50,61,43,65]
