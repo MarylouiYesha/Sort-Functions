041a40f053ddd2b4b7ca5e31232e38c468db8932
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 04:35:05 +0800

    insertion function

--- insertion.py
+++ insertion.py
@@ -1,3 +1,9 @@
 def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while array[j - 1] > array[j] and j > 0:
+            array[j - 1], array[j] = array [j], array[j - 1]
+            j -= 1
+
 
 array =[17,51,52,31,28,47,50,61,43,65]
