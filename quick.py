973f6806299771ca128064a361e3ea43a5bb7d1f
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 16:09:14 +0800

    change method

--- quick.py
+++ quick.py
@@ -1,21 +1,7 @@
-def quick_sort (array, left, right):
-    if left < right:
-        part=part(array, left, right)
-        quick_sort(array, left, part - 1)
-        quick_sort(array, part + 1, right)
-
-def part(array, left, right):
-    i = left
-    j = right - 1
-    pivot = array[right]
-
-    while i < j:
-        while i < right and array[i] < pivot:
-            i += 1
-        while j > left and array[j] >= pivot:
-            j -= 1
-        if i < j:
-            array[1], array[j] = array[j], array[i]
+def quick_sort (sequence):
 
 
-array = [17,51,52,31,28,47,50,61,43,65]
+
+
+print("Array in Ascending Order using Quick Sort")
+[17,51,52,31,28,47,50,61,43,65]
