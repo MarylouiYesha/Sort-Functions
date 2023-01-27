d8a4b9c026c865b836422258ba66b76a3762ec96
MarylouiYesha <yesha.maryloui@gmail.com>
2023-01-27 10:07:00 +0800

    def quick

--- quick.py
+++ quick.py
@@ -1,6 +1,9 @@
 def quick_sort (array, left, right):
     if left < right:
-        
+        part=part(array, left, right)
+        quick_sort(array, left, part - 1)
+        quick_sort(array, part + 1, right)
 
 
-array =[17,51,52,31,28,47,50,61,43,65]
+
+array = [17,51,52,31,28,47,50,61,43,65]
